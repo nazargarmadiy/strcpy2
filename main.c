5c2590cd3e662e052b33550878236b6c40dc854a
nazargarmadiy <vvlviv.garmadiy@gmail.com>
2016-11-28 10:28:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,23 @@
 
 extern void strcpy2(char*a,char*b);
 
+int memcpy2(char*d,char*s,int l)
+{
+    /*magic*/
+    if(d > s)
+    {
+        s += l - 1;
+        d += l - 1;
+        while(l--)
+            *d-- = *s--;
+        return 0;
+    }
+    while(l--)
+        *d++=*s++;
+    return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
     char dst[100] = {'a', 'b', 'c', 'r'};
